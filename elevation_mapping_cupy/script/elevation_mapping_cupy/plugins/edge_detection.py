9b11300e571d83e0fe8cfdc16582fac950b87ca1
Abhay MATHUR <admathur@iitk.ac.in>
2024-05-17 14:57:20 +0200

    edge detection

--- elevation_mapping_cupy/script/elevation_mapping_cupy/plugins/edge_detection.py
+++ elevation_mapping_cupy/script/elevation_mapping_cupy/plugins/edge_detection.py
@@ -3,30 +3,30 @@ from typing import List
 from .plugin_manager import PluginBase
 import cupyx.scipy.ndimage as ndimage
 
-def hough_line_transform(image, theta_res=1, rho_res=1):
-    # Define the range of theta and rho
-    thetas = cp.deg2rad(cp.arange(-90, 90, theta_res))
-    width, height = image.shape
-    diag_len = cp.ceil(cp.sqrt(width * width + height * height))  # maximum rho
-    rhos = cp.linspace(-diag_len, diag_len, cp.ceil(2 * diag_len / rho_res))
+# def hough_line_transform(image, theta_res=1, rho_res=1):
+#     # Define the range of theta and rho
+#     thetas = cp.deg2rad(cp.arange(-90, 90, theta_res))
+#     width, height = image.shape
+#     diag_len = cp.ceil(cp.sqrt(width * width + height * height))  # maximum rho
+#     rhos = cp.linspace(-diag_len, diag_len, cp.ceil(2 * diag_len / rho_res))
 
-    # Initialize accumulator array
-    accumulator = cp.zeros((len(rhos), len(thetas)), dtype=cp.uint64)
+#     # Initialize accumulator array
+#     accumulator = cp.zeros((len(rhos), len(thetas)), dtype=cp.uint64)
 
-    # Indices of the non-zero pixels in the binary image
-    y_idxs, x_idxs = cp.nonzero(image)
+#     # Indices of the non-zero pixels in the binary image
+#     y_idxs, x_idxs = cp.nonzero(image)
 
-    # Vote in the accumulator array
-    for i in range(len(x_idxs)):
-        x = x_idxs[i]
-        y = y_idxs[i]
+#     # Vote in the accumulator array
+#     for i in range(len(x_idxs)):
+#         x = x_idxs[i]
+#         y = y_idxs[i]
 
-        for j in range(len(thetas)):
-            rho = cp.round(x * cp.cos(thetas[j]) + y * cp.sin(thetas[j]))
-            rho_idx = cp.argmin(cp.abs(rhos - rho))
-            accumulator[rho_idx, j] += 1
+#         for j in range(len(thetas)):
+#             rho = cp.round(x * cp.cos(thetas[j]) + y * cp.sin(thetas[j]))
+#             rho_idx = cp.argmin(cp.abs(rhos - rho))
+#             accumulator[rho_idx, j] += 1
 
-    return accumulator, thetas, rhos
+#     return accumulator, thetas, rhos
 
 class EdgeDetection(PluginBase):
     def __init__(self, input_layer_name, algo, sigma=1, min_h = 0.50, **kwargs):
